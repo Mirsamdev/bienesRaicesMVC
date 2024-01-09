860c7be282d1bcaae5bd77dd59093f8292ce7459
Mirsandev <xmirandadevx@gmail.com>
2024-01-09 15:53:53 -0400

    Warning: unlink

--- Router.php
+++ Router.php
@@ -8,22 +8,23 @@ class Router {
     public $rutasPOST = [];
 
     public function get($url, $fn) {
-        $this->rutasPOST[$url] = $fn;
-    }
-
-    public function post($url, $fn) {
       $this->rutasGET[$url] = $fn;
+  }
+  public function post($url, $fn) {
+      $this->rutasPOST[$url] = $fn;
   }
 
     public function comprobarRutas() {
     $urlActual = $_SERVER['PATH_INFO'] ?? '/';
     $metodo = $_SERVER['REQUEST_METHOD'];
 
-    if($metodo === 'GET') {
-        $fn = $this->rutasGET[$urlActual] ?? null;
-    } else {
-        $fn = $this->rutasPOST[$urlActual] ?? null;
-    }
+  if($metodo === 'GET') {
+    $fn = $this->rutasGET[$urlActual] ?? null;
+  } else {
+    $fn = $this->rutasPOST[$urlActual] ?? null;
+  }
+
+
 
     if($fn) {
       // La url existe y hay una funcion asociada 
--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -1,13 +1,16 @@
-<?php 
+<?php
 
 namespace Controllers;
+
 use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
 use Intervention\Image\ImageManagerStatic as Image;
 
-class PropiedadController {
-  public static function index(Router $router) {
+class PropiedadController
+{
+  public static function index(Router $router)
+  {
 
     $propiedades = Propiedad::all();
     $resultado = null;
@@ -18,47 +21,58 @@ class PropiedadController {
     ]);
   }
 
-  public static function crear(Router $router) {
+  public static function crear(Router $router)
+  {
 
     $propiedad = new Propiedad;
     $vendedores = Vendedor::all();
 
-    if($_SERVER['REQUEST_METHOD'] === 'POST') {
-  
+    // Arreglo con mensajes de errores
+    $errores = Propiedad::getErrores();
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
       $propiedad = new Propiedad($_POST['propiedad']);
       //SUBIDA DE ARCHIVOS//
 
       // Generar un nombre unico
-      $nombreImagen = md5( uniqid( rand(), true)) . ".jpg";
+      $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
 
-    //Setear la imagen
-      if($_FILES['propiedad']['tmp_name']['imagen']) {
-      $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800,600);
-      $propiedad->setImagen($nombreImagen);
- }
-    
+      //Setear la imagen
+      if ($_FILES['propiedad']['tmp_name']['imagen']) {
+        $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
+        $propiedad->setImagen($nombreImagen);
+      }
 
 
-    // Validar
-    $errores = $propiedad->validar();
-    $CARPETA_IMAGENES = '../../imagenes/';
-    if(empty($errores)) {
-    // Crear la carpeta para subir imagenes
-    if(!is_dir(CARPETA_IMAGENES)) {
-      mkdir(CARPETA_IMAGENES);
-    };
 
-    // Guardar la imagen
-    $image->save($CARPETA_IMAGENES . $nombreImagen);
+      // Validar
+      $errores = $propiedad->validar();
+      $CARPETA_IMAGENES = '../../imagenes/';
+      if (empty($errores)) {
+        
+        // Crear la carpeta para subir imagenes
+        if (!is_dir(CARPETA_IMAGENES)) {
+          mkdir(CARPETA_IMAGENES);
+        };
 
-    // Guarda en la BD
-   $resultado = $propiedad->guardar();
+        // Guardar la imagen
+        $image->save($CARPETA_IMAGENES . $nombreImagen);
 
+        // Guarda en la BD
+        $propiedad->guardar();
+      }
     }
-  }
 
+    $router->render('propiedades/crear', [
+      'propiedad' => $propiedad,
+      'vendedores' => $vendedores,
+      'errores' => $errores
+    ]);
   }
-  public static function actualizar() {
+  
+  public static function actualizar()
+  {
     echo "actualizar Propiedad";
   }
 }
