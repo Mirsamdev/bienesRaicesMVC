2fefbe1bc862c546839ad8323630325ae8a365ec
Mirsandev <xmirandadevx@gmail.com>
2024-01-17 20:46:09 -0400

    semi-finish

--- Router.php
+++ Router.php
@@ -15,6 +15,14 @@ class Router {
   }
 
     public function comprobarRutas() {
+
+    session_start();
+
+    $auth = $_SESSION['login'] ?? null;
+
+    // Arreglo de rutas protegidas..
+    $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
+
     $urlActual = $_SERVER['PATH_INFO'] ?? '/';
     $metodo = $_SERVER['REQUEST_METHOD'];
 
@@ -24,6 +32,11 @@ class Router {
   } else {
     $urlActual = explode('?',$urlActual)[0];
     $fn = $this->rutasGET[$urlActual] ?? null;
+  }
+
+  // Proteger las rutas
+  if(in_array($urlActual, $rutas_protegidas) && !$auth) {
+    header('Location: /');
   }
 
 
